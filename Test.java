a7d4a8abe4b60ee7b335ba39cd006cb1714802b4
Gaur1234567890 <gauravnamdev02000@outlook.com>
2024-01-25 13:52:31 +0530

    First Program  in java

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String[] args) {
+        int []arr = {10,20,30,40,50};
+        for(int i=0;i<=arr.length-1;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
